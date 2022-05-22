1c12c910e86e6572e82c596599dcdbb8134da85b
Qibin <qbye0405@gmail.com>
2022-05-22 15:20:58 -0400

    commit tree e5a7b537d231877a848530bf4f6aff1daef2fa69

---
+++ index.js
@@ -0,0 +1,13 @@
+document.querySelector('body');
+document.querySelector('/body');
+
+const h1 = document.createELement('h1');
+
+h1.textContent = 'My HTML adventure';
+
+document.querySelector('body').append('h1');
+
+
+
+
+
